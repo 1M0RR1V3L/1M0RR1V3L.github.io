40ba61226f94bc1da0b7818c008d12adc15a7933
Lucas <lsxyz2017@gmail.com>
2024-04-14 16:12:24 -0300

    add second option PWA using pwa builder

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('install', function (event) {
+/*self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open('meu-portfolio-v1').then(function (cache) {
             return cache.addAll([
@@ -18,4 +18,55 @@ self.addEventListener('fetch', function (event) {
             return response || fetch(event.request);
         })
     );
+});
+
+*/
+
+
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
