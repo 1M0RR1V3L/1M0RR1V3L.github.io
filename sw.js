3069cbf6ead66beff7f65bd2db49a3863a2bdb20
Lucas <lsxyz2017@gmail.com>
2024-04-15 20:26:17 -0300

    add new sw.js

--- sw.js
+++ sw.js
@@ -21,6 +21,7 @@ self.addEventListener('fetch', function (event) {
 });
 */
 
+/*
 // This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
@@ -68,8 +69,9 @@ self.addEventListener('fetch', (event) => {
     })());
   }
 });
+*/
 
-// las option
+// last option
 /*
 var staticCacheName = "pwa_dice";
 
@@ -102,3 +104,49 @@ self.addEventListener("fetch", function (event) {
 	);
 });
 */
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE_NAME = "meu-portfolio-v1";
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/script.js',
+                '/img/icon2.jpg'
+            ]);
+        })
+    );
+});
+
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
+);
+
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'script',
+    new workbox.strategies.StaleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'style',
+    new workbox.strategies.StaleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'document',
+    new workbox.strategies.NetworkFirst()
+);
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
