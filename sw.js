7632c73106bc6d9892b25165d827480e5434481c
1m0rr1v3l <lsxyz2017@gmail.com>
2024-07-31 22:32:18 -0300

    update

--- sw.js
+++ sw.js
@@ -4,17 +4,32 @@ self.addEventListener('install', (e) => {
 
   let cache = caches.open(cacheName).then((c) => {
     c.addAll([
-        './index.html',
-        './css/style.css',
-        './js/script.js',
-        './img/github.png',
-        './img/icon.ico',
-        './img/icon-240x256.png',
-        './img/insta.png',
-        './img/linkedin.png',
-        './img/perfil.jpg',
-        './img/select-theme.png'  
-      ]);
+      './index.html',
+      './css/style.css',
+      './js/script.js',
+      './img/github.png',
+      './img/icon.ico',
+      './img/icon-192x192.png',
+      './img/icon-256x256.png',
+      './img/icon-384x384.png',
+      './img/icon-512x512.png',
+      './img/python.svg',
+      './img/js.svg',
+      './img/c.svg',
+      './img/c_hashtag.svg',
+      './img/dj.svg',
+      './img/godot.svg',
+      './img/react.svg',
+      './img/shadcn.svg',
+      './img/tailwind.svg',
+      './img/moon.svg',
+      './img/sun.svg',
+      './img/system.svg',
+      './img/insta.png',
+      './img/linkedin.png',
+      './img/perfil.jpg',
+      './img/select-theme.png'
+    ]);
   });
 
   e.waitUntil(cache);
